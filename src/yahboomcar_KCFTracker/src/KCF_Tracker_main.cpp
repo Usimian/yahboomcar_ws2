d2ae35725572721d5daf1591790590a83417cebb
Marc Wester <marc.wester@gmail.com>
2024-02-13 12:27:16 +0000

    Commit to image new:4.0.7

--- src/yahboomcar_KCFTracker/src/KCF_Tracker_main.cpp
+++ src/yahboomcar_KCFTracker/src/KCF_Tracker_main.cpp
@@ -3,12 +3,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <signal.h>
 
-
-
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
-	rclcpp::init(argc, argv);
-    std::cout<<"start"<<std::endl;
+    rclcpp::init(argc, argv);
+    std::cout << "start" << std::endl;
     rclcpp::spin(std::make_shared<ImageConverter>());
     return 0;
 }
--- src/yahboomcar_astra/launch/colorTracker_X3.launch.py
+++ src/yahboomcar_astra/launch/colorTracker_X3.launch.py
@@ -5,27 +5,30 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    '''astra_node = IncludeLaunchDescription(
+    """astra_node = IncludeLaunchDescription(
     PythonLaunchDescriptionSource([os.path.join(
         get_package_share_directory('astra_camera'),'launch'),'/astra.launch.xml'])
-    )'''
+    )"""
     driver_node = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([os.path.join(
-        get_package_share_directory('yahboomcar_bringup'),'launch'),'/yahboomcar_bringup_X3_launch.py']                            
-    ))
-    colorIdentify_node =  Node(
-                                package='yahboomcar_astra',
-                                node_executable='colorHSV',
-                                node_name='coloridentify'
-                                )
-    '''colorTracker_node = Node(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("yahboomcar_bringup"), "launch"),
+                "/yahboomcar_bringup_X3_launch.py",
+            ]
+        )
+    )
+    colorIdentify_node = Node(package="yahboomcar_astra", node_executable="colorHSV", node_name="coloridentify")
+    """colorTracker_node = Node(
                                 package='yahboomcar_astra',
                                 node_executable='colorTraker',
                                 node_name='colortracker'
-                                )'''
-    return LaunchDescription([#astra_node,
-                              driver_node,
-                              colorIdentify_node,
-                              #colorTracker_node
-                            ])
+                                )"""
+    return LaunchDescription(
+        [  # astra_node,
+            driver_node,
+            colorIdentify_node,
+            # colorTracker_node
+        ]
+    )
